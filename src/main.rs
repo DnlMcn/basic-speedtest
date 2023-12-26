b932046e2c63fd5ed6aa7dc964d5b619f3ffefc6
Danilo Mancini <haomakk.himself@gmail.com>
2023-12-25 23:10:26 -0300

    feat: add speedtesting functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate speedtest_rs;
+
+use speedtest_rs::speedtest::{get_configuration, get_server_list_with_config, get_best_server_based_on_latency, test_download_with_progress_and_config, test_upload_with_progress_and_config};
+
 fn main() {
-    println!("Hello, world!");
+    let mut config = get_configuration().unwrap();
+
+    let server_list = get_server_list_with_config(&config).unwrap();
+
+    let best_server = get_best_server_based_on_latency(
+        &server_list.servers
+    ).unwrap();
+    
+    let download_measurement = test_download_with_progress_and_config(
+        best_server.server, || {}, &mut config
+    ).unwrap();
+
+    let upload_measurement = test_upload_with_progress_and_config(
+        best_server.server, || {}, &mut config
+    ).unwrap();
+
+    println!(
+        "Download speed: {:.2} Megabytes per second",
+        download_measurement.kbps() as f64 / 8000.0 
+    );
+    println!(
+        "Upload speed: {:.2} Megabytes per second",
+        upload_measurement.kbps() as f64 / 8000.0 
+    );
 }
